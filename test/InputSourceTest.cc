dc36b57906da5222d46f54113118593c065d19ee
Chan Yama <pivotalyamachan@gmail.com>
2019-07-25 19:22:57 +0900

    Input Source Test

---
+++ test/InputSourceTest.cc
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+
+#include "InputSource.h"
+
+TEST(stringInputSource, eof_is_false) {
+    auto stringInput = StringInputSource(L"関数、フィボナッチ（番号）");
+
+    EXPECT_FALSE(stringInput.eof());
+}
+
+TEST(stringInputSource, readSingleCharacters) {
+    auto stringInput = StringInputSource(L"関n数");
+
+    EXPECT_EQ(stringInput.peekChar(), L'関');
+    EXPECT_EQ(stringInput.getChar(), L'関');
+    EXPECT_EQ(stringInput.peekChar(), L'n');
+    EXPECT_EQ(stringInput.getChar(), L'n');
+    EXPECT_EQ(stringInput.peekChar(), L'数');
+    EXPECT_FALSE(stringInput.eof());
+
+    // 最後の文字
+    EXPECT_EQ(stringInput.getChar(), L'数');
+    EXPECT_FALSE(stringInput.eof());
+
+    // 終わり
+    EXPECT_EQ(stringInput.peekChar(), L'\0');
+    EXPECT_EQ(stringInput.getChar(), L'\0');
+    EXPECT_TRUE(stringInput.eof());
+
+    // 全部読んだ後
+    EXPECT_EQ(stringInput.peekChar(), L'\0');
+    EXPECT_EQ(stringInput.getChar(), L'\0');
+    EXPECT_TRUE(stringInput.eof());
+}
+
+
