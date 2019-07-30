53f27c5d498f18ea3d1b4b4674ce2379c0744e9e
Chan Yama <pivotalyamachan@gmail.com>
2019-07-30 21:57:43 +0900

    Testing improvements and better 辞書 constructor.

--- src/CoreFunctions.h
+++ src/CoreFunctions.h
@@ -1,10 +1,18 @@
 #ifndef CORE_FUNCTIONS_H
 #define CORE_FUNCTIONS_H
 
+#include "Value.h"
+
 class Environment;
 
 void initModule(Environment *env);
 
 extern const wchar_t *corePinponStarter;
+
+class FunctionNewDictionary : public FunctionValue {
+public:
+    Value *apply(const vector<Value *> &args, Environment *env,
+                 unordered_map<wstring, Value *> *kwargsIn) const override;
+};
 
 #endif
---
+++ test/CoreFunctionsTest.cc
@@ -0,0 +1,25 @@
+#include <CoreFunctions.h>
+#include "gtest/gtest.h"
+
+#include "Tokenizer.h"
+#include "Parser.h"
+#include "Environment.h"
+#include "Context.h"
+
+TEST(coreFunctions, functionNewDictionary) {
+    Context context;
+    Environment env(&context);
+    FunctionNewDictionary f;
+    unordered_map<wstring, Value *> kwargs(
+            {
+                    {wstring(L"hello"), context.newNumberValue(7)}
+            });
+    auto result = f.apply({}, &env, &kwargs);
+
+    EXPECT_EQ(ValueType::DICT, result->type);
+    auto dict = result->toDictionaryValue();
+    EXPECT_EQ(dict->parent, nullptr);
+    EXPECT_EQ(dict->get(L"hello"), context.newNumberValue(7));
+
+    context.cleanup();
+}
